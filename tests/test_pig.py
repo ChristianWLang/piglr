5aca447e5e4cabbdd5d99b40e9e847c2d9747893
Mike Novitsky <novitsky.mike@gmail.com>
2021-05-19 21:20:20 -0400

    Add basic tests

--- tests/test_pig.py
+++ tests/test_pig.py
@@ -33,3 +33,44 @@ def test_init_errors():
         # Check for score < 1
         game = piglr.Game(2, 1, 6, 0)
         game.reset()
+
+
+def test_winner():
+    game = piglr.Game(2, 1, 6, 100)
+    game.reset()
+
+    assert game.target == 100
+
+    score = game._state.score
+    assert score[0] == 0
+    assert score[1] == 0
+
+    winner = game._check_for_winner()
+    assert winner is None
+
+    game._state.score[0] = 100
+    assert game.target == game._state.score[0]
+
+    winner = game._check_for_winner()
+    assert winner is not None
+
+
+def test_bank():
+    game = piglr.Game(2, 1, 6, 100)
+    game.reset()
+
+    score = game._state.score
+    assert score[0] == 0
+    assert score[1] == 0
+
+    assert game._state.bank == 0
+    game._state.turn = 1
+    game._state.rolls = 1
+    game._state.bank = 1
+
+    assert game._state.bank == 1
+
+    game._increment_turn(True)
+
+    assert game._state.bank == 0
+
